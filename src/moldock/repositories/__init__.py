7a6c78f24d3e1ba92285bec83df14813fd841dc8
Bruno Peixoto <brunolnetto@gmail.com>
2026-09-25 15:59:18 -0300

    feat: export DuckLake task repository

--- src/moldock/repositories/__init__.py
+++ src/moldock/repositories/__init__.py
@@ -1,9 +1,11 @@
 from .contracts import ArtifactRepository, TaskRepository
+from .ducklake import DuckLakeTaskRepository
 from .memory import InMemoryArtifactRepository, InMemoryTaskRepository
 
 __all__ = [
     "ArtifactRepository",
     "TaskRepository",
+    "DuckLakeTaskRepository",
     "InMemoryArtifactRepository",
     "InMemoryTaskRepository",
 ]
