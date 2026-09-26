c2f9b7da4ad29db26289d569b4e841496fe27f9c
Bruno Peixoto <brunolnetto@gmail.com>
2026-09-26 13:05:40 -0300

    feat: export pose interaction domain types

--- src/moldock/domain/__init__.py
+++ src/moldock/domain/__init__.py
@@ -7,6 +7,7 @@ from .execution_request import DockingExecutionRequest
 from .experiment import DockingExperiment
 from .protocol import DockingProtocol
 from .failure import ExecutionFailure, FailureKind
+from .interaction import PoseInteraction, PoseInteractionKind
 from .prepared import PreparedLigand, PreparedReceptor
 from .result import Pose, PoseRanking, PoseScore, ScoreKind
 from .retry import RetryPolicy
@@ -37,6 +38,8 @@ __all__ = [
     "PoseMetric",
     "PoseMetricKind",
     "PoseClusterAssignment",
+    "PoseInteraction",
+    "PoseInteractionKind",
     "PoseScore",
     "PoseRanking",
     "ScoreKind",
